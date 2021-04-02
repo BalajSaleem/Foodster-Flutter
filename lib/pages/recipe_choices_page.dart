8a1f65103f6833c6c9293cdf5dd6545d9dd67a6f
balajsaleem <balajsaleem@live.com>
2021-04-02 22:17:01 +0500

    initial recipe selection page almost ready

---
+++ lib/pages/recipe_choices_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseMealsPage extends StatefulWidget {
+  @override
+  _ChooseMealsPageState createState() => _ChooseMealsPageState();
+}
+
+class _ChooseMealsPageState extends State<ChooseMealsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+
+
+  }
+}
